9535d03cb4f08bf9c4ac92bc29655a99f0061131
alaa140101 <alaa140011@gmail.com>
2021-09-11 09:04:04 +0300

    Factories and seeding

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use App\Models\Post;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // DB::table('posts')->insert([
+        //     'title' => Str::random(30),
+        //     'body' => Str::random(300),
+        //     'author' => Str::random(10)
+        // ]);
+        Post::factory()->count(20)->create();
+    }
+}
