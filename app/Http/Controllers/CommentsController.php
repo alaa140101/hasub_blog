c0440fe90368256c8e4a9941514ddf682e7a635f
alaa140101 <alaa140011@gmail.com>
2021-09-17 06:42:27 +0300

    end of Forms

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -2,19 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreComment;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Models\Comment;
 
 class CommentsController extends Controller
 {
-    public function store(POST $post) {
+    public function store(StoreComment $request,POST $post) {
         Comment::create([
             'name' => request('name'),
             'body' => request('body'),
             'post_id' => $post->id
         ]);
-    
+
         return back();
     }
 }
--- resources/views/components/createComment.blade.php
+++ resources/views/components/createComment.blade.php
@@ -1,12 +1,18 @@
-<form action="/posts/{{ $post->id }}/comments" method="post"> 
-  @csrf 
+<form action="/posts/{{ $post->id }}/comments" method="post">
+  @csrf
   <div class="mb-3">
     <label for="name" class="form-label">الاسم</label>
     <input type="text" name="name" class="form-control" id="name">
-  </div>
+</div>
+@error('name')
+    <div class="alert alert-danger">{{$message}}</div>
+@enderror
   <div class="mb-3">
     <label class="form-label" for="body">التعليق</label>
     <textarea name="body" id="body" cols="30" rows="10" class="form-control"></textarea>
-  </div>
+</div>
+@error('body')
+    <div class="alert alert-danger">{{$message}}</div>
+@enderror
   <button type="submit" class="btn btn-primary">حفظ</button>
 </form>
